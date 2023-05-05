d696912dcab94b3d2c45d3b70e4f5dc904e28ac0
paulaperez12 <paula.perez@colorado.edu>
2023-05-05 13:07:15 -0600

    pep8 edits

--- lidar_tools.py
+++ lidar_tools.py
@@ -7,7 +7,6 @@ import rasterstats as rs
 import seaborn as sns
 
 
-
 def calc_height_stats(plots_path, chm_path, insitu_path, id_colname="Plot_ID"):
     """
     Merge LiDAR and insitu stats into one df
@@ -30,10 +29,11 @@ def calc_height_stats(plots_path, chm_path, insitu_path, id_colname="Plot_ID"):
             geojson_out=True)
 
         chm_stats_gdf = gpd.GeoDataFrame.from_features(chm_stats)
-        chm_stats_gdf.rename(columns={"max": "lidar_max", "mean": "lidar_mean"},
-                                inplace=True)
-            
-        return(chm_stats_gdf)
+        chm_stats_gdf.rename(columns={"max": "lidar_max",
+                                      "mean": "lidar_mean"},
+                             inplace=True)
+
+        return (chm_stats_gdf)
 
     def calc_insitu_height_stats(insitu_path):
         """
@@ -47,25 +47,27 @@ def calc_height_stats(plots_path, chm_path, insitu_path, id_colname="Plot_ID"):
             .rename(columns={"max": "insitu_max", "mean": "insitu_mean"})
         )
 
-        return(insitu_df)
-    
+        return (insitu_df)
+
     # Load centroids and buffer
     chm_stats_gdf = calc_lidar_chm_stats(plots_path,
-                                          chm_path)
-    
+                                         chm_path)
+
     # Load insitu data and get max, mean
     insitu_df = calc_insitu_height_stats(insitu_path)
 
     # Make IDs match
-    chm_stats_gdf[id_colname] = [re.sub("[^0-9]", "", id) for id in chm_stats_gdf[id_colname]]
+    chm_stats_gdf[id_colname] = [
+        re.sub("[^0-9]", "", id) for id in chm_stats_gdf[id_colname]]
     insitu_df.index = [re.sub("[^0-9]", "", id) for id in insitu_df.index]
 
     # Merge dfs
     stats_df = chm_stats_gdf.merge(insitu_df,
-                                    right_index=True,
-                                    left_on=id_colname)
-        
-    return(stats_df)
+                                   right_index=True,
+                                   left_on=id_colname)
+
+    return (stats_df)
+
 
 def plot_max_mean_height(stats_df, mean_axis_max, max_axis_max, site_name):
     """
@@ -78,15 +80,15 @@ def plot_max_mean_height(stats_df, mean_axis_max, max_axis_max, site_name):
     ax1.scatter(x=stats_df.lidar_mean, y=stats_df.insitu_mean)
     ax1.plot((0, 1), (0, 1), transform=ax1.transAxes, ls='--', c='k')
     sns.regplot(x="lidar_mean", y="insitu_mean",
-            data=stats_df,
-            color="purple",
-            ax=ax1)
+                data=stats_df,
+                color="purple",
+                ax=ax1)
 
     ax2.set(xlim=(0, max_axis_max), ylim=(0, max_axis_max), aspect="equal",
             title=site_name + " - Max Canopy Height")
     ax2.scatter(x=stats_df.lidar_max, y=stats_df.insitu_max)
     ax2.plot((0, 1), (0, 1), transform=ax2.transAxes, ls='--', c='k')
     sns.regplot(x="lidar_max", y="insitu_max",
-            data=stats_df,
-            color="purple",
-            ax=ax2)
+                data=stats_df,
+                color="purple",
+                ax=ax2)
